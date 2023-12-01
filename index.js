74b82eb38be63583eb9e9d4628ba540e89cf347a
Afariphi <agung.afr10@gmail.com>
2023-12-01 14:43:31 +0700

    update index.js

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const initializeSocket = require('./Back-End/Chat/socketController');
 const uploadController = require('./Back-End/Upload/uploadController');
 
 const app = express();
-const server = http.createServer(app);
-
 const port = 3000;
 
 app.use(bodyParser.json());
@@ -16,8 +14,6 @@ app.use(bodyParser.json());
 app.use('/auth', authController);
 app.use('/profile', profileController);
 app.use('/upload', uploadController);
-
-initializeSocket(server);
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
